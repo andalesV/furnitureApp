340fd77a3888beb3690970c464992ed547c9d26e
Victor Andales <victor.andales@usjr.edu.ph>
2018-05-02 00:27:26 +0800

    commit setup configurations

---
+++ app/src/main/java/com/assignment/coding/furnitureapp/main/MainPresenter.java
@@ -0,0 +1,8 @@
+package com.assignment.coding.furnitureapp.main;
+
+/**
+ * Created by victor on 01/05/2018.
+ */
+
+public class MainPresenter {
+}
---
+++ app/src/main/java/com/assignment/coding/furnitureapp/views/IMainView.java
@@ -0,0 +1,8 @@
+package com.assignment.coding.furnitureapp.views;
+
+/**
+ * Created by victor on 01/05/2018.
+ */
+
+public interface IMainView {
+}
